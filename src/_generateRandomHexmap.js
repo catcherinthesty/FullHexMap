92067cbcf98def8a20aef8ece40a26c630ce1550
John Heinsen <john.r.heinsen@gmail.com>
2023-08-10 14:14:48 -0400

    This should all be working for now. Terrain has been expanded a bit. Still need to add a list of descriptions.

--- src/_generateRandomHexmap.js
+++ src/_generateRandomHexmap.js
@@ -1,5 +1,6 @@
 terrainTypes = require('./terrain.js');
 terrainList = Object.keys(terrainTypes)
+terrainList.splice(terrainList.indexOf('blank'),1)
 gridOut = { rows: 25, columns: 20, tiles: [],}
 for(let col=0; col<gridOut.columns; col++) {
     for(let row=0; row<gridOut.rows; row++) {
